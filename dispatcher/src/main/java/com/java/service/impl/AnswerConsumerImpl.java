d0a5174e83843893c3a6c0dcf5bd25dc130cd11f
BusyDizzy <antontk@gmail.com>
2023-06-20 22:59:52 +0700

    UpdateController add; UpdateProducer add; MessageUtils add

---
+++ dispatcher/src/main/java/com/java/service/impl/AnswerConsumerImpl.java
@@ -0,0 +1,10 @@
+package com.java.service.impl;
+
+import com.java.service.AnswerConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+public class AnswerConsumerImpl implements AnswerConsumer {
+    @Override
+    public void consume(SendMessage sendMessage) {
+    }
+}
